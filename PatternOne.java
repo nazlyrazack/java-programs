5be8b177db2ea09541cecb1c1c48a16370327a91
Nazly Razack <nazly@bromlays.com>
2018-07-23 20:19:49 +0530

    initial commit

---
+++ PatternOne.java
@@ -0,0 +1,17 @@
+public class PatternOne{
+	public static void main(String[] args)
+	{
+
+		int i,j;
+
+		for(i=5;i>=1;i--)
+		{
+			for(j=i;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("\n");
+		}
+
+	}
+}
